2333d1d8baceb370852ab6db85abe637018293d8
Viktoriya Elizaryan <velizaryan@thrivedetect.com>
2021-03-22 13:54:58 +0500

    litle fix

--- lesson2/VElizaryan/race_emulator.js
+++ lesson2/VElizaryan/race_emulator.js
@@ -11,26 +11,26 @@ function* generator() {
 Array.prototype.sum = function() {
     let sum = 0;
     for (let i = 0; i < this.length; i++){
-        sum += Number(this[i])
+        sum += Number(this[i]);
     }
-    return sum
+    return sum;
 }
 
 let car1Array = [];
 let car2Array = [];
 
-console.log("___________________________________")
-console.log("Старт гонки")
+console.log("___________________________________");
+console.log("Старт гонки");
 
 while (car1Array.sum() < 100 && car2Array.sum() < 100) {
-    car1Array.push(generator().next().value)
-    car2Array.push(generator().next().value)
+    car1Array.push(generator().next().value);
+    car2Array.push(generator().next().value);
     console.log("скорость первой машины: " + car1Array.sum(), "скорость второй машины: " + car2Array.sum())
     if (car1Array.sum() > 105 || car2Array.sum() > 105) {
-        console.log("скорость одной из машин больше 105, начинаем гонку заного")
-        console.log("                                   ")
-        console.log("___________________________________")
-        console.log("Старт гонки")
+        console.log("скорость одной из машин больше 105, начинаем гонку заного");
+        console.log("                                   ");
+        console.log("___________________________________");
+        console.log("Старт гонки");
         car1Array = [];
         car2Array = [];
     }
@@ -39,8 +39,8 @@ while (car1Array.sum() < 100 && car2Array.sum() < 100) {
 console.log("___________________________________")
 console.log("Финиш гонки")
 if (car1Array.sum() > car2Array.sum())
-    console.log("Победила первая машина со скоростью: " + car1Array.sum(), "скорость второй машины: " + car2Array.sum())
+    console.log("Победила первая машина со скоростью: " + car1Array.sum(), "скорость второй машины: " + car2Array.sum());
 if (car2Array.sum() > car1Array.sum())
-    console.log("Победила вторая машина со скоростью: " + car2Array.sum(), "скорость первой машины: " + car1Array.sum())
+    console.log("Победила вторая машина со скоростью: " + car2Array.sum(), "скорость первой машины: " + car1Array.sum());
 if (car2Array.sum() === car1Array.sum())
-    console.log("Ничья, скорость машин: " + car1Array.sum())
+    console.log("Ничья, скорость машин: " + car1Array.sum());
